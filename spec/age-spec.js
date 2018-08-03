6d1f8a573f13687d3c3d96202fdca0db35302819
“Nick <amellocello@msn.com>
2018-08-03 12:49:43 -0700

    It will pass all tests.

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -4,38 +4,64 @@ describe('Age', function() {
 
   it('will check for valid birthday', function() {
     let birthday = new Date('1993-03-23');
-    let newAge = new Age(birthday, "male", 4,3);
+    let newAge = new Age(birthday, "male", 4, 3);
     expect(newAge.isValidBirthday()).toEqual(true);
   });
   it('will return the difference in seconds between birthday and a date', function() {
     let birthday = new Date('1993-03-23');
     let futureDate = new Date('1993-03-27')
-    let newAge = new Age(birthday, "male", 4,3);
+    let newAge = new Age(birthday, "male", 4, 3);
     expect(newAge.timeDifference(futureDate)).toEqual(345600);
   });
   it('will return the age in seconds for Mercury', function() {
     let birthday = new Date('1995-04-19');
     let futureDate = new Date('1995-05-02');
-    let newAge = new Age(birthday, "female", 5,5);
+    let newAge = new Age(birthday, "female", 5, 5);
     expect(newAge.toMercury(futureDate)).toEqual(4680000);
   });
   it('will return the age in seconds for Venus', function() {
     let birthday = new Date('1995-04-19');
     let futureDate = new Date('1995-05-02');
-    let newAge = new Age(birthday, "female", 5,5);
+    let newAge = new Age(birthday, "female", 5, 5);
     expect(newAge.toVenus(futureDate)).toEqual(1811613);
   });
   it('will return the age in seconds for Mars', function() {
     let birthday = new Date('1995-04-19');
     let futureDate = new Date('1995-05-02');
-    let newAge = new Age(birthday, "female", 5,5);
+    let newAge = new Age(birthday, "female", 5, 5);
     expect(newAge.toMars(futureDate)).toEqual(597447);
   });
   it('will return the age in seconds for Jupiter', function() {
     let birthday = new Date('1995-04-19');
     let futureDate = new Date('1995-05-02');
-    let newAge = new Age(birthday, "female", 5,5);
+    let newAge = new Age(birthday, "female", 5, 5);
     expect(newAge.toJupiter(futureDate)).toEqual(94705);
   });
-
+  it('will return the number of years in the timespan', function() {
+    let birthday = new Date('1993-03-23');
+    let futureDate = new Date('2018-08-03');
+    let newAge = new Age(birthday, "male", 4, 3);
+    expect(newAge.toYears(newAge.timeDifference(futureDate))).toEqual(25);
+  });
+  it('will return the remaining number of months in the timespan after removing years', function() {
+    let birthday = new Date('1993-03-23');
+    let futureDate = new Date('2018-08-03');
+    let newAge = new Age(birthday, "male", 4, 3);
+    expect(newAge.toMonths(newAge.timeDifference(futureDate))).toEqual(4);
+  });
+  it('will return the life expectancy of a person', function() {
+    let birthday = new Date('1993-03-23');
+    let newAge = new Age(birthday, "male", 4, 3);
+    expect(newAge.lifeExpectancy()).toEqual(79);
+  })
+  it('will return the life expectancy of a person and output a message', function() {
+    let birthday = new Date('1993-03-23');
+    let newAge = new Age(birthday, "male", 4, 3);
+    expect(newAge.compareLife()).toEqual('Your life expectancy is 79 years.');
+  })
+  it('will return the life expectancy of a person and output a message', function() {
+    let birthday = new Date('1893-03-23');
+    let newAge = new Age(birthday, "male", 4, 3);
+    expect(newAge.compareLife()).toEqual("You've lived 46 years and 5 months over your life expectancy of 79 years!");
+  })
 });
--- src/age.js
+++ src/age.js
@@ -15,7 +15,7 @@ export class Age {
     return (date - this.birthday)/1000;
   }
 
-  //Conversions to Mercury, Venus, Mars, and Jupiter years
+  //Conversions to Mercury, Venus, Mars, and Jupiter seconds
   toMercury(date){
     let inputtedDate = date;
     return Math.round(this.timeDifference(inputtedDate)/0.24);
@@ -31,5 +31,33 @@ export class Age {
   toJupiter(date){
     let inputtedDate = date;
     return Math.round(this.timeDifference(inputtedDate)/11.86);
+  }
+
+  //Conversions to Years and Months
+  toYears(seconds){
+    return Math.floor(seconds/31536000);
+  }
+  toMonths(seconds){
+    return Math.floor((seconds%31536000)/2628000);
+  }
+
+  //Returns the life expectancy or how long they are over their life expectancy
+  lifeExpectancy(){
+    let baseLife = 77;
+    if(this.gender === "female"){
+      baseLife = baseLife + 7;
+    }
+    let healthYears = (this.health - 3)*2;
+    let exerciseYears = (this.exercise - 3)*2;
+    return baseLife + healthYears + exerciseYears;
+  }
+  compareLife(){
+    if(this.lifeExpectancy()*31536000 >= this.timeDifference(Date.now())){
+      return "Your life expectancy is " + this.lifeExpectancy() + " years.";
+    }
+    else
+    {
+      return "You've lived " + this.toYears(this.timeDifference(Date.now())-this.lifeExpectancy()*31536000) + " years and " + this.toMonths(this.timeDifference(Date.now())-this.lifeExpectancy()*31536000) + " months over your life expectancy of " + this.lifeExpectancy() + " years!";
+    }
   }
 }
